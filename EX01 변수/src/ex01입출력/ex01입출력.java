b977ab3a35d9bff5ca7fbe6768deeef76107b232
black4305 <solomon819@naver.com>
2025-02-05 11:51:10 +0900

    프로젝트, 패키지, 클래스 생성 / 주석 & 출력 학습

---
+++ EX01 변수/src/ex01입출력/ex01입출력.java
@@ -0,0 +1,23 @@
+package ex01입출력;
+
+public class ex01입출력 {
+	
+	public static void main(String[] args) {
+		// 글씨 키우기 : command + shift + 더하기
+		// 초록색 글씨 : 주석 (코드 실행에 영향을 끼치지 않는 일종의 메모장)
+		
+		System.out.println("출력하고 싶은 내용 작성");
+		// println : 다음 실행 문장부터 개행이 됨
+		
+		System.out.print("Hello World 123");
+		// command + S : 저장하는 단축기
+		// 언제나 코드를 작성하고 나서 "저장 필수" -> 실행 버튼 클릭
+		// 출력문 단축기 : syso + ctrl + space + enter!
+		
+		System.out.println("안녕하세요~ 반갑습니다!");
+		
+		// 변수(variable)
+		// : 데이터를 담아둘 수 있는 공간(메모리 공간, 저장 공간)
+	}
+	
+}
